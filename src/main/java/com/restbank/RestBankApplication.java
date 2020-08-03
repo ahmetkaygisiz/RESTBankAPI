412cd6335b0f7e7f41a327f09cd7c01665c4fd9a
ahmetkaygisiz <ahmetkaygisiz17@gmail.com>
2020-08-03 23:46:04 +0300

    github'ta proje olusturmak bitirmenin yarisidir

---
+++ src/main/java/com/restbank/RestBankApplication.java
@@ -0,0 +1,13 @@
+package com.restbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restbank/RestBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
